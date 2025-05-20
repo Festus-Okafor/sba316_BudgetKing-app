54e533163c0626891e7232454767f79f91f99326
festus_okafor <okaforfestus465@gmail.com>
2025-05-20 06:06:59 -0400

    working on the logic

--- app.js
+++ app.js
@@ -13,20 +13,37 @@ document.body.style.backgroundColor = "black";
  
  //accessing the submit button
   const submitBtn = document.getElementById("submitBtn")
-  const new_income = document.getElementById("newincome")
+
+
+   //accessing the first child of the income parent 
+  const new_income = document.getElementById("new_income")
+  const firstOfIncome = new_income.firstChild;
+  //accessing the first child of the expense parent 
+   const new_expense = document.getElementById("new_expense")
+  const firstOfExpense = new_expense.firstChild;
+  
+
   const selectElement = document.getElementById("select")
   const selectedValue =  selectElement.value;
   //console.log(selectedValue)
     //adding event listener to it
     // comparing the user intension choice
   submitBtn.addEventListener('click', (e) =>{
-     if( selectedValue === Income){
+     if(selectedValue === "Expense"){
         //add transaction to new income and save
-       new_income.push(input.textContent = value);
-       e.preventDefault;
+      // new_income.push(input.textContent = value);
+          firstOfExpense.textContent = e.target.value
+          e.preventDefault();
+    
      }
+     else if(selectedValue === "Income"){
+          firstOfIncome.textContent = e.target.value
+          e.preventDefault()
+     }  })
+     
+  
 
-  })
+
   
 
 
