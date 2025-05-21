66c16f9e407813f0e176f4ab3d300f6336bfcd38
festus_okafor <okaforfestus465@gmail.com>
2025-05-21 06:59:15 -0400

    html and css structures set

--- app.js
+++ app.js
@@ -1,51 +1,125 @@
 
 const body = document.createElement('body');
 document.body.style.backgroundColor = "black";
-
-
+const updateIncomeBtn = document.getElementById("updateIncomeBtn")
+const updateExpenseBtn = document.getElementById("updateExpenseBtn")
+const updateBalanceBtn = document.getElementById("updateBalanceBtn")
+const cont1Screen = document.getElementById('.cont1')
  const display = document.getElementById("display")
  const input = document.getElementById("amountInput")
+const submitBtn = document.getElementById("submitBtn")
+const new_income = document.getElementById("new_income")
+  const new_expense = document.getElementById("new_expense")
+  const selectElement = document.getElementById("select")
+  const selectedValue =  selectElement.value;
 
  //display = "";
- input.addEventListener('input' , function(e){
+ /* input.addEventListener('input' , function(e){
      display.textContent = e.target.value;
- })
+ }) */
+
+/* submitBtn.addEventListener('click' ,(e) =>{
+   if(selectedValue === "Income"){
+      const value = input.value;
+      const myIncome = Number(new_income.innerText)+ Number(value)
+      new_income.innerText = myIncome;
+}
+      
+})
+submitBtn.addEventListener('click' ,(e) =>{
+   if(selectedValue === "Expense"){
+      const value = input.value;
+      const myExpense = Number(new_expense.innerText)+ Number(value)
+      new_expense.innerText = myExpense;
+}}
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
  
  //accessing the submit button
-  const submitBtn = document.getElementById("submitBtn")
+  
 
 
    //accessing the first child of the income parent 
-  const new_income = document.getElementById("new_income")
-  const firstOfIncome = new_income.firstChild;
-  //accessing the first child of the expense parent 
-   const new_expense = document.getElementById("new_expense")
-  const firstOfExpense = new_expense.firstChild;
   
 
-  const selectElement = document.getElementById("select")
-  const selectedValue =  selectElement.value;
+  //accessing the first child of the expense parent 
+ 
+  
+
   //console.log(selectedValue)
     //adding event listener to it
     // comparing the user intension choice
-  submitBtn.addEventListener('click', (e) =>{
-     if(selectedValue === "Expense"){
-        //add transaction to new income and save
-      // new_income.push(input.textContent = value);
-          firstOfExpense.textContent = e.target.value
-          e.preventDefault();
+
+/* 
+    let incomeOutput = []; 
+    let sum = 0;
+  input.addEventListener('input', (e) =>{
+     if(selectedValue === "Income"){
+        input.value = new_expense;
+         sum += new_expense = newIncomeAmt
+         incomeOutput.push(newIncomeAmt);
+         //let new_expenseVal = new_expense.push(lastOfExpense.textContent = e.target.value)
+         lastOfIncome.textContent = incomeOutput;
+
+     }}) */
     
-     }
-     else if(selectedValue === "Income"){
-          firstOfIncome.textContent = e.target.value
+    /*  else if(selectedValue === "Income"){
+          lastOfIncome.textContent = e.target.value
           e.preventDefault()
      }  })
-     
+      */
+   /* 
+  //update income 
+  let incomeArr = []
+    updateIncomeBtn.addEventListener('click' , (e) => {
+           <form id="updateIncome">
+           <input id="description" placeholder="type description here"/>
+           <input id="amount" placeholder="type Amount here"/>
+           <button type="submit">click</button>
+          </form>
+
+    })
+ updateExpenseBtn.addEventListener('click' , (e) => {
+           <form id="updateIncome">
+           <input id="description" placeholder="type description here"/>
+           <input id="amount" placeholder="type Amount here"/>
+           <button type="submit">click</button>
+          </form>
+    })
+     updateExpenseBtn.addEventListener('click' , (e) => {
+           <form id="updateIncome">
+           <input id="description" placeholder="type description here"/>
+           <input id="amount" placeholder="type Amount here"/>
+           <button type="submit">click</button>
+          </form>
+    })
+
+    updateBalanceBtn.addEventListener('click' , (e) => {
+           <form id="updateIncome">
+           <input id="description" placeholder="type description here"/>
+           <input id="amount" placeholder="type Amount here"/>
+           <button type="submit">click</button>
+          </form>
+    })
   
-
-
-  
-
+ */
 
 
 
